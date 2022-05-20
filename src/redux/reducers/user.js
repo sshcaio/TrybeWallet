30ec07a2af6ff48cb568ae56dcf869618e68c890
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-05-20 05:31:55 -0300

    [start] Initial commit
    Setting up dependencies and redux folders and files.

---
+++ src/redux/reducers/user.js
@@ -0,0 +1,19 @@
+import { USER } from "../actions/index";
+
+const INITIAL_STATE = {
+  user: '',
+};
+
+function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case USER:
+      return {
+        ...state,
+        user: action.payload,
+      }
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
---
+++ src/redux/reducers/wallet.js
@@ -0,0 +1,19 @@
+import { WALLET } from "../actions/index";
+
+const INITIAL_STATE = {
+  wallet: '',
+};
+
+function walletReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case WALLET:
+      return {
+        ...state,
+        wallet: action.payload,
+      }
+    default:
+      return state;
+  }
+}
+
+export default walletReducer;
