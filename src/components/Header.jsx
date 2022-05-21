154198e902d2027f13b1d61d1ba1f50eab07b5ad
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-05-21 04:27:08 -0300

    [lint] Third requirement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,22 @@ import { connect } from 'react-redux';
 class Header extends Component {
   render() {
     const { email } = this.props;
-    console.log(email)
     return (
       <header>
         <div data-testid="email-field">{ email }</div>
         <div data-testid="total-field">{ 0 }</div>
         <div data-testid="header-currency-field">BRL</div>
       </header>
-    )
+    );
   }
 }
 
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
 const mapStateToProps = (rootReducer) => ({
   email: rootReducer.user.email,
-})
+});
 
 export default connect(mapStateToProps)(Header);
