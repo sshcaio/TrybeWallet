1707a3550f5dd354a223fc7ff8e6122a1e658b8b
Caio de Sousa (Sony) <sonyhellish@gmail.com>
2022-05-23 16:49:56 -0300

    [lint]

--- src/helpers/ExpenseAdding.js
+++ src/helpers/ExpenseAdding.js
@@ -1,7 +1,5 @@
-const expenseAdding = (expense, coin) => {
-  return (
-    Number(expense.value) * expense.exchangeRates[coin].ask
-  )
-};
+const expenseAdding = (expense, coin) => (
+  Number(expense.value) * expense.exchangeRates[coin].ask
+);
 
 export default expenseAdding;
